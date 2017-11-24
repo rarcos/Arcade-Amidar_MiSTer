edae658d261bc5f232f4c2d28f0ff3fbeaf9ebc7
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:38:30 +0800

    Load ROM from SD

---
+++ releases/build_rom_Amidar.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=amidars.zip
+set ifiles=am2d+am2e+am2f+am2h+am2j+am2l+am2m+am2p+2716.a5+2716.a6+amidarus.5c+amidarus.5d
+set  ofile=a.amidar.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
